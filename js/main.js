c29a64e5c6987251898b2d872d398444279b3704
shogenji <ruirui@ruirui.jp>
2024-11-07 10:25:09 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -98,31 +98,14 @@ function loop() {
                 dst.data[(y * dst.width + x) * 4 + 3] = 255;
         }
     }
-    // anaglyph
-    // for (let y = 0; y < dst.height / 2; y++) {
-    //   for (let x = 0; x < dst.width; x++) {
-    //       let yy = Math.floor(y / interval) * interval;
-    //           dst.data[(y * dst.width + x) * 4 + 0] = src.data[(yy * dst.width + x) * 4 + 0];
-    //           dst.data[(y * dst.width + x) * 4 + 1] = src.data[(yy * dst.width + x) * 4 + 1] * 0;
-    //           dst.data[(y * dst.width + x) * 4 + 2] = src.data[(yy * dst.width + x) * 4 + 2] * 0;
-    //           dst.data[(y * dst.width + x) * 4 + 3] = 255;
-    //   }
-    // }
-    // for (let y = dst.height / 2; y < dst.height; y++) {
-    //   for (let x = 0; x < dst.width; x++) {
-    //       let yy = Math.floor(y / interval) * interval;
-    //           dst.data[(y * dst.width + x) * 4 + 0] = src.data[(yy * dst.width + x) * 4 + 0] * 0;
-    //           dst.data[(y * dst.width + x) * 4 + 1] = src.data[(yy * dst.width + x) * 4 + 1] * 0;
-    //           dst.data[(y * dst.width + x) * 4 + 2] = src.data[(yy * dst.width + x) * 4 + 2];
-    //           dst.data[(y * dst.width + x) * 4 + 3] = 255;
-    //   }
-    // }
 
     // decoded_ctx.drawImage(dst, 0, 0);
     // decoded_ctx.putImageData(dst, 0, 0);
     // decoded_ctx.putImageData(dst, 0, 0, 0, 0, offscreen.height * (canvas.height / canvas.width), offscreen.height);
     // decoded_ctx.putImageData(dst, 0, 0, 0, 0, canvas.width, canvas.height);
-    decoded_ctx.drawImage(dst, 0, 0, canvas.width * 2, canvas.height * 2, 0, 0, canvas.width, canvas.height);
+    offscreen_ctx.putImageData(dst, 0, 0, 0, 0, offscreen.width, offscreen.height);
+    // decoded_ctx.drawImage(offscreen, 0, 0);
+    decoded_ctx.drawImage(offscreen, 0, 0, offscreen.width, offscreen.height, 0, 0, canvas.width, canvas.height);
     //context.drawImage(image, 0, 0, beforeWidth, beforeHeight, 0, 0, afterWidth, afterHeight);
 
     // const bigCanvas = document.getElementById("big");
